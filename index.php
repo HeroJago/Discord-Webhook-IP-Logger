e98ebb78805c80f1029b2b99fe7d9505014e8ffc
HeroJago <97869023+HeroJago@users.noreply.github.com>
2024-09-24 17:06:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta property="og:title" content="heker kebon nanas" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="fotoduit.jpg" />
-        <meta property="og:url" content="index.php" />
-        <meta property="og:description" content="Heker nasa terkuat di kebon Nanas" />
-        <title>izin Test Web</title>
-    </head>
-    <body>
-        <img src="fotoduit.jpg" alt="Foto Duit">
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="heker kebon nanas" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="fotoduit.jpg" />
+    <meta property="og:url" content="index.php" />
+    <meta property="og:description" content="Heker nasa terkuat di kebon Nanas" />
+    <title>izin Test Web</title>
+</head>
+<body>
+    <p>Halo bang ini cuma test web</p>
+    <img src="fotoduit.jpg" alt="Foto Duit">
+</body>
 </html>
