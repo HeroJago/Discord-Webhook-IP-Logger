c407c77f9a57ecfc699e6a77245e288dc1d4cea7
HeroJago <97869023+HeroJago@users.noreply.github.com>
2024-09-24 17:00:15 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:title" content="heker kebon nanas" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="fotoduit.jpg" />
+        <meta property="og:url" content="index.php" />
+        <meta property="og:description" content="Heker nasa terkuat di kebon Nanas" />
+        <title>izin Test Web</title>
+    </head>
+    <body>
+        <img src="fotoduit.jpg" alt="Foto Duit">
+    </body>
+</html>
